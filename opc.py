42f3003996eb5b862949d4cb2df57b40114b6438
Ahmad Poureskandari <ahmad.p.92@gmail.com>
2023-06-09 09:55:49 +0330

    minimal connection

--- opc.py
+++
@@ -1,8 +0,0 @@
-from asyncua import Client
-
-async with Client(url='opc.tcp://localhost:4840/freeopcua/server/') as client:
-    while True:
-        #Do
-        node = client.get_node('i=85')
-        value = await node.read_value()
-
